32961b2376749c5d090c9057d49350f9f2cc26a6
krumeluren <9928liqv@gmail.com>
2022-05-24 21:43:26 +0200

    Add Log manager

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+    private readonly ILoggerManager logger;
+
+    public WeatherForecastController(ILoggerManager logger)
+    {
+        this.logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<string> Test()
+    {
+        logger.LogInfo("Here is info message from the controller.");
+        logger.LogDebug("Here is debug message from the controller.");
+        logger.LogWarn("Here is warn message from the controller.");
+        logger.LogError("Here is error message from the controller.");
+        return new string[] { "value1", "value2" };
+    }
+
+    
+}
