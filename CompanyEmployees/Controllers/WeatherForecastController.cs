54cd3f7b2bd5c042efa514686c781af6ae647c5b
krumeluren <9928liqv@gmail.com>
2022-05-24 23:11:35 +0200

    Fix

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -2,9 +2,6 @@ using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers;
-
-namespace CompanyEmployees.Controllers
-{
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
@@ -12,7 +9,7 @@ public class WeatherForecastController : ControllerBase
     private static readonly string[] Summaries = new[]
     {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    };
     private readonly ILoggerManager logger;
 
     public WeatherForecastController(ILoggerManager logger)
@@ -21,23 +18,24 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<string> Test()
-        public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+    }
+    public IEnumerable<string> Test()
+
     {
         logger.LogInfo("Here is info message from the controller.");
         logger.LogDebug("Here is debug message from the controller.");
         logger.LogWarn("Here is warn message from the controller.");
         logger.LogError("Here is error message from the controller.");
         return new string[] { "value1", "value2" };
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
 
-    
+    }
 }
