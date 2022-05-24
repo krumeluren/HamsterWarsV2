77eddefcbe5bc778880f461c2f7d221ef21f4c32
krumeluren <9928liqv@gmail.com>
2022-05-25 01:41:50 +0200

    OA
    Set up OA and add entities

---
+++ Domain/Entities/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Models
+{
+    public class Employee
+    {
+        [Column("EmployeeId")]
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public Guid CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+}
