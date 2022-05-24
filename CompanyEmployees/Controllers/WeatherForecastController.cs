aa47685f7a3f02ce13df22677f9c3f3f10f0e17b
krumeluren <9928liqv@gmail.com>
2022-05-24 23:16:19 +0200

    Fix

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -17,17 +17,7 @@ public class WeatherForecastController : ControllerBase
         this.logger = logger;
     }
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
 
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray();
-    }
     public IEnumerable<string> Test()
 
     {
