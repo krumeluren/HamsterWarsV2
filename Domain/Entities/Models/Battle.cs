7c33a92d47ffdb533b3fb1c02dcc4da3b0cdbec2
krumeluren <9928liqv@gmail.com>
2022-05-30 16:43:48 +0200

    Add Entities
    Add Hamster and Battle entity

---
+++ Domain/Entities/Models/Battle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Models;
+public class Battle
+{
+    public int Id { get; set; }
+    public DateTime TimeOfPost { get; set; }
+    public int? WinnerHamsterId { get; set; }
+    
+    [ForeignKey("WinnerHamsterId")]
+    public virtual Hamster? WinnerHamster { get; set; }
+    public int? LoserHamsterId { get; set; }
+    [ForeignKey("LoserHamsterId")]
+    public virtual Hamster? LoserHamster { get; set; }
+    
+    public Battle()
+    {
+        TimeOfPost = DateTime.Now;
+    }
+}
---
+++ Domain/Entities/Models/Hamster.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace Domain.Entities.Models;
+public class Hamster
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string? FavFood { get; set; }
+    public string? Loves { get; set; }
+    public string? ImgName { get; set; }
+    public int? Wins { get; set; } = 0;
+    public int? Losses { get; set; } = 0;
+    public int? Games { get; set; } = 0;
+
+    [InverseProperty("WinnerHamster")]
+    public ICollection<Battle>? BattlesWon {get; private set; }
+    [InverseProperty("LoserHamster")]
+    public ICollection<Battle>? BattlesLost { get; set; }
+}
