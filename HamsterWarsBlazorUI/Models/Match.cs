361e0f053b2e670eb37afe97e239ac92a87ca65c
krumeluren <9928liqv@gmail.com>
2022-06-15 02:10:27 +0200

    Add Match model

---
+++ HamsterWarsBlazorUI/Models/Match.cs
@@ -0,0 +1,14 @@
+﻿namespace Presentation.HamsterWarsBlazorUI.Models;
+
+public class Match
+{
+    public int Id { get; set; }
+
+    public int WinnerHamsterId { get; set; }
+    public int LoserHamsterId { get; set; }
+
+    public DateTime TimeOfPost { get; set; }
+    
+    public Hamster? WinnerHamster { get; set; }
+    public Hamster? LoserHamster { get; set; }
+}
