f5cc12fb22dd04d06357fe82257252c618c613d0
krumeluren <9928liqv@gmail.com>
2022-08-30 17:53:33 +0200

    Add Xamarin app project

---
+++ HamsterWarsXamarin/HamsterWarsXamarin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using HamsterWarsXamarin.Models;
+using HamsterWarsXamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HamsterWarsXamarin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
---
+++ HamsterWarsXamarinUI/HamsterWarsXamarinUI/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HamsterWarsXamarinUI.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HamsterWarsXamarinUI.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
